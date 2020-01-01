c9dfde437b509440388457bb0d7749c36a7f2159
Cipri <popescu.ciprian98@gmail.com>
2020-01-01 18:14:13 +0200

    Firebase Login

--- app/src/main/java/local/cipri/mobilelearningapp/loginFragments/LoginFragment.java
+++ app/src/main/java/local/cipri/mobilelearningapp/loginFragments/LoginFragment.java
@@ -4,6 +4,7 @@ package local.cipri.mobilelearningapp.loginFragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import local.cipri.mobilelearningapp.MainActivity;
 import local.cipri.mobilelearningapp.R;
@@ -22,8 +30,14 @@ public class LoginFragment extends Fragment {
     private Button btnLogin;
     private Button btnRegister;
 
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private static final String emailRegEx = "[^@]+@[^\\.]+\\..+";
+    private static final String LOGGED_USER = "logged_user";
 
-    public LoginFragment() {}
+
+    public LoginFragment() {
+    }
 
 
     @Override
@@ -34,31 +48,64 @@ public class LoginFragment extends Fragment {
         return view;
     }
 
-    private void initForm(View view){
+    private void initForm(View view) {
         btnLogin = view.findViewById(R.id.btnLogin);
         btnRegister = view.findViewById(R.id.btnRegister);
         etEmail = view.findViewById(R.id.etEmail);
         etPassword = view.findViewById(R.id.etPassword);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
         initBtnLogin();
         initBtnRegister();
     }
 
-    private void initBtnLogin(){
-        btnLogin.setOnClickListener(new View.OnClickListener(){
+    private void initBtnLogin() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getContext(), MainActivity.class));
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                if (validateCredentials(email, password))
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                firebaseUser = firebaseAuth.getCurrentUser();
+                                Toast.makeText(getContext(), R.string.success_login, Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(getContext(),MainActivity.class);
+                                intent.putExtra(LOGGED_USER, firebaseUser);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getContext(), R.string.error_login, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                else {
+                    //todo: beter handling, with red underlines etc etc
+                    Toast.makeText(getContext(), R.string.error_login, Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
 
-    private void initBtnRegister(){
-        btnRegister.setOnClickListener(new View.OnClickListener(){
+    protected static boolean validateCredentials(String email, String password) {
+        if (email != null
+                && password != null
+                && !email.isEmpty()
+                && !password.isEmpty()
+                && password.length() >= 6
+                && email.matches(emailRegEx))
+            return true;
+        return false;
+    }
+
+    private void initBtnRegister() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getActivity().getSupportFragmentManager()
                         .beginTransaction()
-                        .replace(R.id.login_frame_container,new RegisterFragment())
+                        .replace(R.id.login_frame_container, new RegisterFragment())
                         .commit();
             }
         });
--- app/src/main/java/local/cipri/mobilelearningapp/loginFragments/RegisterFragment.java
+++ app/src/main/java/local/cipri/mobilelearningapp/loginFragments/RegisterFragment.java
@@ -4,6 +4,7 @@ package local.cipri.mobilelearningapp.loginFragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import local.cipri.mobilelearningapp.LoginActivity;
 import local.cipri.mobilelearningapp.MainActivity;
@@ -20,10 +28,12 @@ import local.cipri.mobilelearningapp.R;
 public class RegisterFragment extends Fragment {
 
     private Button btnRegister;
-    private EditText etName;
     private EditText etEmail;
     private EditText etPassword;
     private EditText etPasswordConfirm;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     public RegisterFragment() {
     }
@@ -37,22 +47,45 @@ public class RegisterFragment extends Fragment {
         return view;
     }
 
-    private void initForm(View view){
-        etName = view.findViewById(R.id.etUserName);
+    private void initForm(View view) {
         btnRegister = view.findViewById(R.id.btnNewRegister);
         etEmail = view.findViewById(R.id.etNewEmail);
         etPassword = view.findViewById(R.id.etNewPassword);
         etPasswordConfirm = view.findViewById(R.id.etNewPasswordConfirm);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
         initBtnRegister();
     }
 
-    private void initBtnRegister(){
-        btnRegister.setOnClickListener(new View.OnClickListener(){
+    private void initBtnRegister() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getContext(), LoginActivity.class));
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                String passconf = etPasswordConfirm.getText().toString();
+                if (validateCredentials(email, password, passconf)) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                firebaseUser = firebaseAuth.getCurrentUser();
+                                Toast.makeText(getContext(), R.string.success_register, Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(getContext(), LoginActivity.class));
+                            } else {
+                                Toast.makeText(getContext(), R.string.error_register_default, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
             }
         });
+    }
+
+    private boolean validateCredentials(String email, String pass, String confirm) {
+        if (LoginFragment.validateCredentials(email, pass))
+            return true;
+        return false;
     }
 
 }
